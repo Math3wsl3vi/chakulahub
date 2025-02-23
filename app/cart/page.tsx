263e5c21381c6d8fda2bf06642c5fa8473ed3da7
math3wsl3vi <levimathews9841@gmail.com>
2025-02-24 00:27:07 +0300

    cart changes updatequantity

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -6,7 +6,7 @@ import { useToast } from "@/hooks/use-toast";
 import jsPDF from "jspdf";
 
 const CartPage = () => {
-  const { cart, removeFromCart } = useCartStore();
+  const { cart, removeFromCart,updateQuantity } = useCartStore();
   const { toast } = useToast();
   const [phoneNumber, setPhoneNumber] = useState("");
   const [polling, setPolling] = useState(false);
@@ -112,6 +112,21 @@ const CartPage = () => {
               <div>
                 <h3>{item.name}</h3>
                 <p>Ksh {item.price} x {item.quantity}</p>
+              </div>
+              <div className="flex items-center border p-1 rounded-md">
+                <button
+                  onClick={() => updateQuantity(item.id, item.quantity - 1)}
+                  className="px-2 py-1 text-lg"
+                >
+                  −
+                </button>
+                <span className="mx-3 text-lg">{item.quantity}</span>
+                <button
+                  onClick={() => updateQuantity(item.id, item.quantity + 1)}
+                  className="px-2 py-1 text-lg"
+                >
+                  +
+                </button>
               </div>
               <button onClick={() => removeFromCart(item.id)} className="text-red-500">Remove</button>
             </div>
